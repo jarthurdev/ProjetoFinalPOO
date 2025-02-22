77b5d4c302e2e0d478636467baedecac736ecc7b
RuhtraOnCode <arthurnowork@gmail.com>
2025-02-22 17:26:51 -0300

    pagamento e PagamentoController ajustes

--- src/locadora/controller/PagamentoController.java
+++ src/locadora/controller/PagamentoController.java
@@ -1,5 +1,31 @@
 package locadora.controller;
 
+import java.util.ArrayList;
+
+import locadora.model.Pagamento;
+
 public class PagamentoController {
+
+    ArrayList<Pagamento> pagamentos = new ArrayList<Pagamento>();
+
+    public void addPagamento(double valor, int id, String metodo){
+        Pagamento pagamento = new Pagamento(valor, id, metodo);
+        pagamentos.add(pagamento);
+    }
+
+    public void removePagamento(int id){
+        for (Pagamento pagamento : pagamentos) {
+            if(pagamento.getId() == id){
+                pagamentos.remove(pagamento);
+                break;
+            }
+        }
+    }
+
+    public void listarPagamentos(){
+        for (Pagamento pagamento : pagamentos) {
+            System.out.println(pagamento.toString());
+        }
+    }
 
 }
--- src/locadora/model/Pagamento.java
+++ src/locadora/model/Pagamento.java
@@ -1,5 +1,55 @@
 package locadora.model;
 
+import java.time.LocalDate;
+
 public class Pagamento {
 
+    private double valor;
+    private int id;
+    private String metodo;
+    private LocalDate dataPagamento;
+
+    protected Locacao locacao;
+    
+
+    public Pagamento(double valor, int id, String metodo){
+        this.valor = valor;
+        this.id = id;
+        this.metodo = metodo;
+        this.dataPagamento = LocalDate.now();
+    }
+
+    public double getValor() {
+        return valor;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getMetodo() {
+        return metodo;
+    }
+    public void setMetodo(String metodo) {
+        this.metodo = metodo;
+    }
+
+    public String toString(){
+        return "Valor: " + valor + "\n ID: " + id + "\n Método: " + metodo +
+        "Data: "+ dataPagamento + "\n\n";
+    }
+
+    public double calcularPagamento(){
+        if (dataPagamento.isAfter(locacao.getDataDevolucao())) {
+            if (locacao.veiculo instanceof Carro) {
+                return valor * 1.1; // 10% multa
+            } else if (locacao.veiculo instanceof Moto) {
+                return valor * 1.05; // 5% multa
+            } else {
+                return valor * 1.2; // 20% multa 
+            }
+        } else {
+            return valor;
+        }
+    }
 }
