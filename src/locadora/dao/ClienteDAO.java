3eef1ceca8695eb055af76e08b85a25971df30ca
brunnofdev <brunnofdev@gmail.com>
2025-03-06 16:01:39 -0300

    ClienteDAO corrigido

--- src/locadora/dao/ClienteDAO.java
+++ src/locadora/dao/ClienteDAO.java
@@ -1,13 +1,7 @@
 package locadora.dao;
 import locadora.model.Cliente;
-import locadora.model.Veiculo;
-import locadora.controller.ClienteController;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.JsonSyntaxException;
 import java.lang.reflect.Type;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -27,28 +21,34 @@ public class ClienteDAO {
         gson = new GsonBuilder().setPrettyPrinting().create();
     }
 
-    public void salvarLista(ClienteController clienteController) {
+    public void salvarLista(ArrayList<Cliente> listaCLiente) {
 
         try (Writer writer = new FileWriter(arquivo)) {
-            gson.toJson(clienteController.retornarListaClientes(), writer);
+            gson.toJson(listaCLiente, writer);
             System.out.println("Lista de clientes salva em " + arquivo);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void carregarLista() {
+    public ArrayList<Cliente> carregarLista() {
         try (Reader reader = new FileReader(arquivo)) {
 
             Type tipoLista = new TypeToken<ArrayList<Cliente>>(){}.getType();
-            ArrayList<Cliente> lista = new ArrayList<>();
+            ArrayList<Cliente> lista = new ArrayList<Cliente>();
     
             lista = gson.fromJson(reader, tipoLista);
             
+            if (lista==null){
+                return new ArrayList<Cliente>();
+            }
+
             System.out.println("Lista de clientes carregada com sucesso.");
+            return lista;
         } catch (IOException e) {
             System.err.println("Erro ao carregar o arquivo: " + e.getMessage());
             e.printStackTrace();
+            return new ArrayList<Cliente>();
         }
     }    
 
